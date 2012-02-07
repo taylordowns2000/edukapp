ed438ef845725d2f8897fcb05ae61496d6710851
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-07 17:05:50 +0000

    Deploy a Solr search engine for searching the actual widget metadata, and handlers for pushing data into it from Wookie, and for querying it from Edukapp

---
+++ trunk/src/main/java/uk/ac/edukapp/repository/Widget.java
@@ -0,0 +1,74 @@
+package uk.ac.edukapp.repository;
+
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.Serializable;
+import org.apache.solr.client.solrj.beans.Field;
+
+/**
+ * POJO for representing widgets returned from SOLR repository
+ */
+public class Widget implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Field
+	public String title;
+	@Field
+	public String id;
+	@Field
+	public String uri;
+	@Field
+	public String icon;
+	@Field
+	public String description;
+
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUri() {
+		return uri;
+	}
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+	public String getIcon() {
+		return icon;
+	}
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
+
+
+
