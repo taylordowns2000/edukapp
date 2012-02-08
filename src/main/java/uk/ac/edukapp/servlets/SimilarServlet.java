b5022cd4c8425898a442196f5d1f25656fcd4faa
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-08 11:41:29 +0000

    Added license headers and attribution

--- src/main/java/uk/ac/edukapp/servlets/SimilarServlet.java
+++ src/main/java/uk/ac/edukapp/servlets/SimilarServlet.java
@@ -1,3 +1,19 @@
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.ac.edukapp.servlets;
 
 import java.io.IOException;
@@ -12,6 +28,13 @@ import uk.ac.edukapp.model.Widgetprofile;
 import uk.ac.edukapp.renderer.MetadataRenderer;
 import uk.ac.edukapp.service.WidgetProfileService;
 
+/**
+ * Similar API endpoint
+ * 
+ * TODO replace with Wink/JAX-RS
+ * @author scott.bradley.wilson@gmail.com
+ *
+ */
 public class SimilarServlet extends HttpServlet{
 
 	/* (non-Javadoc)
