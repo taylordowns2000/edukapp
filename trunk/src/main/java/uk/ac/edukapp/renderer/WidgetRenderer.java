ce64b7427c5cc5e0d644d9f3fbd777cc0ce49e8f
anastluc@gmail.com <anastluc@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-10 17:37:34 +0000

    set up widget renderer as a Singleton

--- trunk/src/main/java/uk/ac/edukapp/renderer/WidgetRenderer.java
+++ trunk/src/main/java/uk/ac/edukapp/renderer/WidgetRenderer.java
@@ -1,21 +1,59 @@
 package uk.ac.edukapp.renderer;
 
+import java.io.IOException;
+
+import org.apache.wookie.connector.framework.User;
+import org.apache.wookie.connector.framework.WidgetInstance;
 import org.apache.wookie.connector.framework.WookieConnectorException;
 import org.apache.wookie.connector.framework.WookieConnectorService;
+
+import uk.ac.edukapp.repository.SolrConnector;
 
 
 
 public class WidgetRenderer {
-	public static String render(String url){
-		
-		try {
-			WookieConnectorService conn = new WookieConnectorService("http://widgets.open.ac.uk:8080/wookie/", "TEST", "myshareddata");
-		} catch (WookieConnectorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		
-		return null;
+  
+  private static WidgetRenderer renderer = new WidgetRenderer();
+  
+  private WookieConnectorService conn;
+  
+  
+  private WidgetRenderer(){
+    try {
+      conn = new WookieConnectorService("http://widgets.open.ac.uk:8080/wookie/", "TEST", "myshareddata");      
+    }catch(WookieConnectorException wce){
+      wce.printStackTrace();
+    }
+  }  
+  
+  
+  public static WidgetRenderer getInstance() {
+    return renderer;
+  }
+  
+  /**
+   * Returns html markup of widget instance as an iframe
+   * @param the id (typically a uri) of widget to render
+   * @return html-ready code as a string
+   */
+	public String render(String uri){
+	  String html = "";
+	  WidgetInstance widgetInstance = null;
+	  try {
+	    conn.setCurrentUser(new User("edukapp-front-page","edukapp-front-page"));
+	    widgetInstance = conn.getOrCreateInstance(uri);
+	  }catch(IOException ioe){
+	    ioe.printStackTrace();
+	    return null;
+	  }catch(WookieConnectorException wce){
+	    wce.printStackTrace();
+	    return null;
+	  }
+	  html+="<p>"+uri+"</p>";
+	  html+="<iframe src=\""+widgetInstance.getUrl()+"\"";
+	  html+=" width=\""+"100"+"\"";
+	  html+=" height=\""+"100"+"\"";
+	  html+="></iframe>";
+	  return html;
 	}
 }
