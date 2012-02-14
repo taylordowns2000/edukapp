58d3dfcf94472ea099a62ba7c719cdeb5692efe0
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-14 11:21:12 +0000

    Ignore linked widgetprofiles when rendering activity and tags to JSON

--- src/main/java/uk/ac/edukapp/model/Tag.java
+++ src/main/java/uk/ac/edukapp/model/Tag.java
@@ -2,6 +2,9 @@ package uk.ac.edukapp.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 import java.util.List;
 
 
@@ -45,6 +48,7 @@ public class Tag implements Serializable {
 		this.tagtext = tagtext;
 	}
 
+	@JsonIgnore
 	public List<Widgetprofile> getWidgetprofiles() {
 		return this.widgetprofiles;
 	}
