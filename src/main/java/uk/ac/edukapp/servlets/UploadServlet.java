de9fc7f2930ceea361fd882aab877f7f7c8cd14a
anastluc <anastluc@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-17 01:25:57 +0000

    Upload operations if widget then act as a proxy and forward request to wookie rest service /wookie/widgets if gadget then add it to db

---
+++ src/main/java/uk/ac/edukapp/servlets/UploadServlet.java
@@ -0,0 +1,204 @@
+package uk.ac.edukapp.servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.MultipartPostMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.ByteArrayPartSource;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import uk.ac.edukapp.model.Accountinfo;
+import uk.ac.edukapp.model.Useraccount;
+import uk.ac.edukapp.model.Widgetprofile;
+import uk.ac.edukapp.util.MD5Util;
+
+
+
+
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class UploadServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+  private Log log;
+  private static final File FILE_UPLOAD_TEMP_DIRECTORY = new File(System.getProperty("java.io.tmpdir"));
+  private int intMaxFileUploadSize = 25 * 1024 * 1024;//25 mb is more than enough
+
+  /**
+   * @see HttpServlet#HttpServlet()
+   */
+  public UploadServlet() {
+    super();
+    log = LogFactory.getLog(UploadServlet.class);
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+  }
+
+  /**
+   * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+   *      response)
+   */
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+	  
+	  log.info("upload servlet");
+
+    if (ServletFileUpload.isMultipartContent(request)) {//file attached =>widget invoke wookie/widgets rest service
+    
+    	log.info("request having a file attached - post it to /wookie/widgets");
+    	
+    	DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+    	// Set factory constraints
+    	diskFileItemFactory.setSizeThreshold(intMaxFileUploadSize);
+    	diskFileItemFactory.setRepository(FILE_UPLOAD_TEMP_DIRECTORY);
+    	// Create a new file upload handler
+    	ServletFileUpload servletFileUpload = new ServletFileUpload(diskFileItemFactory);
+    	try {
+      		// Get the multipart items as a list
+      		List<FileItem> listFileItems = (List<FileItem>) servletFileUpload.parseRequest(request);
+      		// Create a list to hold all of the parts
+      		List<Part> listParts = new ArrayList<Part>();
+      		// Iterate the multipart items list
+      		for(FileItem fileItemCurrent : listFileItems) {
+      			// If the current item is a form field, then create a string part
+      			if (fileItemCurrent.isFormField()) {
+      				StringPart stringPart = new StringPart(
+      						fileItemCurrent.getFieldName(), // The field name
+      						fileItemCurrent.getString()     // The field value
+      				);
+      				log.info("request string part:"+fileItemCurrent.getFieldName()+":"+fileItemCurrent.getString());
+      				// Add the part to the list
+      				listParts.add(stringPart);
+      			} else {
+      				// The item is a file upload, so we create a FilePart
+      				FilePart filePart = new FilePart(
+      						fileItemCurrent.getFieldName(),    // The field name
+      						new ByteArrayPartSource(
+      								fileItemCurrent.getName(), // The uploaded file name
+      								fileItemCurrent.get()      // The uploaded file contents
+      						)
+      				);
+      				// Add the part to the list
+      				listParts.add(filePart);
+      			}
+      		}
+      		PostMethod postMethod = new PostMethod("http://localhost:8080/wookie/widgets");
+      		MultipartRequestEntity multipartRequestEntity = new MultipartRequestEntity(
+    																listParts.toArray(new Part[] {}),
+    																postMethod.getParams()
+    															);
+
+            postMethod.setRequestEntity(multipartRequestEntity);
+            
+            HttpClient client = new HttpClient();
+            
+            //add the basic http authentication credentials
+            Credentials defaultcreds = new UsernamePasswordCredentials("java", "java");
+            client.getState().setCredentials(new AuthScope("localhost", 8080, AuthScope.ANY_REALM), defaultcreds);            
+            
+            
+            int status = client.executeMethod(postMethod);
+            
+            log.info("post execution return status:"+status);
+            
+    	}catch(FileUploadException fue){
+    		fue.printStackTrace();
+    	}catch(Exception e){
+    		e.printStackTrace();
+    	}
+          
+    } else {
+
+      // get parameters
+      String uploadurl = null;
+      String gadgetName = null;
+
+      uploadurl = request.getParameter("uploadurl");
+      gadgetName = request.getParameter("gadget-name");
+
+      EntityManagerFactory factory = Persistence
+          .createEntityManagerFactory("edukapp");
+      EntityManager em = factory.createEntityManager();
+
+      /*-----------*/
+      Widgetprofile gadget = null;
+      try {
+        em.getTransaction().begin();
+
+        gadget = new Widgetprofile();
+        gadget.setName(gadgetName);
+        byte one = 1;
+        gadget.setW3cOrOs(one);
+        gadget.setWidId(uploadurl);
+        em.persist(gadget);
+
+        log.info("Gadget created with id:" + gadget.getId());
+
+        
+      } catch (Exception e) {
+        e.printStackTrace();
+        // em.getTransaction().rollback();
+        doForward(request, response, "/upload.jsp?error=1");
+      }
+      /*----------*/
+
+      em.getTransaction().commit();
+      em.close();
+      factory.close();
+
+      if (gadget != null) {
+        doForward(request, response, "/widget.jsp?id=" + gadget.getId());
+      } else {
+        doForward(request, response, "/upload.jsp?error=1");
+      }
+    }
+
+  }
+
+  
+  private void doForward(HttpServletRequest request,
+      HttpServletResponse response, String jsp) throws ServletException,
+      IOException {
+    RequestDispatcher dispatcher = getServletContext()
+        .getRequestDispatcher(jsp);
+    dispatcher.forward(request, response);
+  }
+
+}
--- src/main/webapp/upload.jsp
+++ src/main/webapp/upload.jsp
@@ -13,12 +13,73 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<link rel="stylesheet" href="css/layout.css00000000000" type="text/css" />
 <link rel="stylesheet" href="css/reset.css" type="text/css" />
 <link rel="stylesheet" href="css/layout.css" type="text/css" />
 <link rel="stylesheet" href="css/header.css" type="text/css" />
 <link rel="stylesheet" href="css/main.css" type="text/css" />
 <link rel="stylesheet" href="css/footer.css" type="text/css" />
+<script src="scripts/jquery.min.js"></script>
+<script src="scripts/jquery-ui-1.8.17.custom.min.js"></script>
+<script>
+	$(document).ready(function() {
+		$('#upload-forms-tabs').tabs();
+	});
+</script>
+<style>
+.ui-tabs {
+	position: relative;
+	padding: .2em;
+	zoom: 1;
+}
+/* position: relative prevents IE scroll bug (element with position: relative inside container with overflow: auto appear as "fixed") */
+.ui-tabs .ui-tabs-nav {
+	margin: 0;
+	padding: .2em .2em 0;
+}
+
+.ui-tabs .ui-tabs-nav li {
+	list-style: none;
+	float: left;
+	position: relative;
+	top: 1px;
+	margin: 0 .2em 1px 0;
+	border-bottom: 0 !important;
+	padding: 0;
+	white-space: nowrap;
+}
+
+.ui-tabs .ui-tabs-nav li a {
+	float: left;
+	padding: .5em 1em;
+	text-decoration: none;
+}
+
+.ui-tabs .ui-tabs-nav li.ui-tabs-selected {
+	margin-bottom: 0;
+	padding-bottom: 1px;
+}
+
+.ui-tabs .ui-tabs-nav li.ui-tabs-selected a,.ui-tabs .ui-tabs-nav li.ui-state-disabled a,.ui-tabs .ui-tabs-nav li.ui-state-processing a
+	{
+	cursor: text;
+}
+
+.ui-tabs .ui-tabs-nav li a,.ui-tabs.ui-tabs-collapsible .ui-tabs-nav li.ui-tabs-selected a
+	{
+	cursor: pointer;
+}
+/* first selector in group seems obsolete, but required to overcome bug in Opera applying cursor: text overall if defined elsewhere... */
+.ui-tabs .ui-tabs-panel {
+	display: block;
+	border-width: 0;
+	padding: 1em 1.4em;
+	background: none;
+}
+
+.ui-tabs .ui-tabs-hide {
+	display: none !important;
+}
+</style>
 <title>EDUKApp</title>
 </head>
 <body>
@@ -51,26 +112,53 @@
 				<%
 				  if (isAuthenticated) {
 				%>
+				<div id="upload-forms-tabs">
+					<ul>
+						<li><a href="#upload-form">W3C widget</a></li>
+						<li><a href="#upload-gadget-form">Gadget</a></li>
+					</ul>
+					<div class="clear"></div>
+					<div id="upload-form">
+						<form method="post"
+							action="/upload"
+							name="upform" enctype="multipart/form-data">
+							<p>Select a widget archive (.zip/.wgt) to upload:</p>
 
-				<div id="upload-form">
-					<form method="post"
-						action="http://localhost:8080/wookie/admin/WidgetAdminServlet?operation=UPLOADWIDGET"
-						name="upform" enctype="multipart/form-data">
-						<p>Select a widget archive (.zip/.wgt) to upload:</p>
+							<p>
+								<input type="file" name="uploadfile" size="50" class="button" />
+							</p>
+							<p>
+								<input class="button" type="submit" name="Submit"
+									value="Publish" /> <input class="button" type="submit"
+									name="Reset" value="Clear" />
+							</p>
+						</form>
+					</div>
 
-						<p>
-							<input type="file" name="uploadfile" size="50" class="button" />
-						</p>
-						<p>
-							<input class="button" type="button" name="Submit" value="Publish" />
-							<input class="button" type="reset" name="Reset" value="Clear" />
-						</p>
-
-					</form>
+					<div id="upload-gadget-form">
+						<form method="post" action="/upload" name="gadgetupform">
+							<p>Enter the url of gadget (.xml)</p>
+							<p>
+								Gadget name: <input type="text" name="gadget-name" size="50"
+									class="button" />
+							</p>
+							<p>
+								Url: <input type="text" name="uploadurl" size="50"
+									class="button" />
+							</p>
+							<p>
+								<input class="button" type="submit" name="Submit"
+									value="Publish" />
+							</p>
+						</form>
+					</div>
 				</div>
+				<div class="clear"></div>
 
-				<div id="upload-gadget-form"></div>
+
+
 			</div>
+			<div class="clear"></div>
 
 			<%
 			  } else {//user not logged in
@@ -81,6 +169,7 @@
 			  }
 			%>
 		</div>
+		<div class="clear"></div>
 	</div>
 
 
