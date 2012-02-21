d2a4b420bd77a1aa71f34b98d5416945d764039c
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-21 11:12:32 +0000

    include "read more" button in featured widgets linking to the widget profile page

--- trunk/src/main/webapp/scripts/featured.js
+++ trunk/src/main/webapp/scripts/featured.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
         $(div).attr("class",itemClass);
         $(div).append("<div class='pull-left' style='border:6px gray solid; margin-left:75px'>"+data[i].renderInfo)+"</div>";
         $(div).append("<div class='pull-left' style='padding:10px'><h3>"+data[i].widgetProfile.name+"</h3><p>"+data[i].widgetProfile.description.description+"</p></div>");
+        $(div).append("<a class='btn btn-inverse' href='widget.jsp?id="+data[i].widgetProfile.id+"'>Read more</a>");
         $(div).appendTo(".carousel-inner");
         itemClass="item";
     }
