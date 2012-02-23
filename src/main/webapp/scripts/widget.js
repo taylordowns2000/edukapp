66f33a47b2baeffd58e70340098e6b3b29f05a78
anastluc@gmail.com <anastluc@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-23 17:01:40 +0000

    related widgets appear as thumbnails

--- src/main/webapp/scripts/widget.js
+++ src/main/webapp/scripts/widget.js
@@ -211,7 +211,16 @@ $(document)
 										//
 										$.getJSON('/similar?uri='+ widgetUri,function(similar) {
                                             for ( var i = 0; i < similar.length; i++) {
-                                                $("<li><a href='widget.jsp?id="+similar[i].id+"'>"+ similar[i].name + "</a></li>")
+                                                $("<li class='span2'>"+
+                                                	'<div class="thumbnail">'+
+                                                	'	<a href="widget.jsp?id='+similar[i].id+'">'+ similar[i].name + '</a>'+
+                                                	'	<div class="caption">'+
+                                                	'		<h5>'+similar[i].description.description+'</h5>'+
+                                                	'		<p></p>'+
+                                                	'		<p><a href="widget.jsp?id='+similar[i].id+'" class="btn btn-primary">Read more >></a></p>'+
+                                                	'	</div>'+
+                                                	'</div>'+                                                		
+                                                	'</li>')
                                                     .hide()
                                                     .appendTo("#related-widgets")
                                                     .fadeIn("slow");
