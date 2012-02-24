28edcebce45f3cfa348d750d7e21337f99a1ebec
anastluc@gmail.com <anastluc@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-24 15:19:10 +0000

    Tag servlet to return (as JSON) the widgets tagged with

---
+++ trunk/src/main/java/uk/ac/edukapp/servlets/TagServlet.java
@@ -0,0 +1,80 @@
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.edukapp.servlets;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import uk.ac.edukapp.model.Tag;
+import uk.ac.edukapp.model.Widgetprofile;
+import uk.ac.edukapp.renderer.MetadataRenderer;
+import uk.ac.edukapp.service.WidgetProfileService;
+
+/**
+ * Tags api endpoint
+ * 
+ * @author anastluc
+ * 
+ */
+public class TagServlet extends HttpServlet {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
+   * javax.servlet.http.HttpServletResponse)
+   */
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    String tagid = req.getParameter("id");
+
+    EntityManagerFactory factory = (EntityManagerFactory) getServletContext()
+        .getAttribute("emf");
+
+    EntityManager em = factory.createEntityManager();
+    WidgetProfileService widgetProfileService = new WidgetProfileService(
+        getServletContext());
+    Tag tag = null;
+
+    if (tagid != null && tagid.trim().length() > 0) {
+      tag = em.find(Tag.class, Integer.parseInt(tagid));
+      List<Widgetprofile> widgetsTaggedWith = widgetProfileService
+          .findWidgetProfilesForTag(tag);
+      OutputStream out = resp.getOutputStream();
+      MetadataRenderer.render(out, widgetsTaggedWith);
+      out.flush();
+      out.close();
+    }
+
+  }
+
+}
---
+++ trunk/src/main/webapp/tag.jsp
@@ -0,0 +1,42 @@
+<%@page import="uk.ac.edukapp.renderer.WidgetRenderer"%>
+<%@page import="uk.ac.edukapp.model.Widgetprofile"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ page
+	import="org.apache.wookie.connector.framework.*,uk.ac.edukapp.util.*,uk.ac.edukapp.model.Useraccount,java.util.*,javax.persistence.*,javax.persistence.EntityManager,javax.persistence.EntityManagerFactory,org.apache.commons.logging.Log,org.apache.commons.logging.LogFactory,uk.ac.edukapp.renderer.*"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<%@ include file='components/imports.jsp'%>
+<%@ include file='components/login_check.jsp'%>
+
+<title>EDUKApp</title>
+</head>
+<body>
+
+	<%@ include file='/components/header.jsp'%>
+
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span3">
+				<div class="well sidebar-nav">
+					<ul class="nav nav-list">
+						<li class="nav-header">Sidebar</li>
+						<li><a href="#"><i class="icon-question-sign"></i>Something
+								here...</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="span9">
+			
+			
+			
+				<%@ include file="/components/footer.jsp"%>
+			</div>
+
+		</div>
+		<!-- end of page-wrapper -->
+</body>
+</html>
