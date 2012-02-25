0b015d207318ad3671c5c394efda5ebb9ebd6edf
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-25 16:25:06 +0000

    Move "get parameters" script to util.js and include in each page - this is where we put general utility scripts

--- src/main/webapp/scripts/search.js
+++ src/main/webapp/scripts/search.js
@@ -99,15 +99,3 @@ function displaySearchResults(data){
 function createPaginationLinks(number_of_results, current_page){
 
 }
-
-function getParameterByName(name)
-{
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regexS = "[\\?&]" + name + "=([^&#]*)";
-  var regex = new RegExp(regexS);
-  var results = regex.exec(window.location.search);
-  if(results == null)
-    return "";
-  else
-    return decodeURIComponent(results[1].replace(/\+/g, " "));
-}
