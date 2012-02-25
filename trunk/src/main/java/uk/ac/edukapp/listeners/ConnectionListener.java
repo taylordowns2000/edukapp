66e9520a97e8b262d7c3d42adb249c2013f5a684
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-02-25 23:44:23 +0000

    remove unused imports

--- trunk/src/main/java/uk/ac/edukapp/listeners/ConnectionListener.java
+++ trunk/src/main/java/uk/ac/edukapp/listeners/ConnectionListener.java
@@ -2,16 +2,6 @@ package uk.ac.edukapp.listeners;
 
 import javax.persistence.*;
 import javax.servlet.*;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.RealmSecurityManager;
-import org.apache.shiro.realm.Realm;
-
-import uk.ac.edukapp.model.Useraccount;
-import uk.ac.edukapp.security.SecurityRealm;
-import uk.ac.edukapp.service.UserAccountService;
  
 public class ConnectionListener implements ServletContextListener {
 
