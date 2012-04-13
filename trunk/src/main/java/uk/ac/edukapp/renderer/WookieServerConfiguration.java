07448ca3179ba1c3e999787f50e10498314adc91
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-04-13 14:53:01 +0000

    Enabled WookieServerConfiguration to also handle Wookie admin credentials and API authorization (may move this out of the renderer package)

--- trunk/src/main/java/uk/ac/edukapp/renderer/WookieServerConfiguration.java
+++ trunk/src/main/java/uk/ac/edukapp/renderer/WookieServerConfiguration.java
@@ -1,7 +1,13 @@
 package uk.ac.edukapp.renderer;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.log4j.Logger;
 import org.apache.wookie.connector.framework.WookieConnectorException;
 import org.apache.wookie.connector.framework.WookieConnectorService;
@@ -10,8 +16,12 @@ public class WookieServerConfiguration {
 	
 	static Logger _logger = Logger.getLogger(WookieServerConfiguration.class.getName());
 	
+	// Defaults
 	private String url = "http://localhost:8080/wookie";
 	private String apiKey = "TEST";
+	private String username = "java";
+	private String password = "java";
+	
 	private static WookieServerConfiguration instance;
 	
 	private WookieServerConfiguration(){
@@ -19,7 +29,8 @@ public class WookieServerConfiguration {
 			PropertiesConfiguration properties = new PropertiesConfiguration("store.properties");
 			if (properties.containsKey("wookie.server.location")) url = properties.getString("wookie.server.location");
 			if (properties.containsKey("wookie.server.apiKey")) apiKey = properties.getString("wookie.server.apiKey");
-			
+			if (properties.containsKey("wookie.server.username")) username = properties.getString("wookie.server.username");
+			if (properties.containsKey("wookie.server.password")) password = properties.getString("wookie.server.password");
 		} catch (ConfigurationException e) {
 			//
 			// No store properties found, so default to "localhost"
@@ -35,6 +46,27 @@ public class WookieServerConfiguration {
 
 	public String getWookieServerLocation(){
 		return url;
+	}
+	
+	/**
+	 * Get an auth scope for Wookie
+	 * @return the AuthScope for Wookie
+	 * @throws MalformedURLException
+	 */
+	public AuthScope getAuthScope() throws MalformedURLException{
+		URL url = new URL(getWookieServerLocation());
+		int port = url.getPort();
+		String host = url.getHost();
+		AuthScope authScope = new AuthScope(host, port, AuthScope.ANY_REALM);
+		return authScope;
+	}
+	
+	/**
+	 * Get a set of credentials to use with Wookie
+	 * @return a Credentials object
+	 */
+	public Credentials getCredentials(){
+		return new UsernamePasswordCredentials(username, password);
 	}
 	
 	public WookieConnectorService getWookieConnector(String sharedDataKey) throws WookieConnectorException{
