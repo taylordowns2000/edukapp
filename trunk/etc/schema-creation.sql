bb6b1a7032a4f526c69711dcf1a6bb599e65394c
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-04-17 09:19:18 +0000

    Removing "old" schema definitions - these can be retrieved from history if needed.

--- trunk/etc/schema-creation.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS useraccount (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username varchar(20) NOT NULL,
-  email varchar(100) NOT NULL,
-  password varchar(256) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
