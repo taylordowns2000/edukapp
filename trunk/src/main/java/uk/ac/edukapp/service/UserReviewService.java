5d53911f69a5d10b8811aefdb4c04a0cea643539
scott.bradley.wilson@gmail.com <scott.bradley.wilson@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-06-28 17:51:22 +0000

    Added ability to pull in external reviews of widgets, and to publish user reviews as they are submitted

--- trunk/src/main/java/uk/ac/edukapp/service/UserReviewService.java
+++ trunk/src/main/java/uk/ac/edukapp/service/UserReviewService.java
@@ -16,6 +16,7 @@
 
 package uk.ac.edukapp.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,12 +24,23 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.servlet.ServletContext;
 
+import org.apache.log4j.Logger;
+
+import uk.ac.bolton.spaws.ParadataManager;
+import uk.ac.bolton.spaws.model.IReview;
+import uk.ac.bolton.spaws.model.ISubmission;
+import uk.ac.bolton.spaws.model.impl.Actor;
+import uk.ac.bolton.spaws.model.impl.Review;
+import uk.ac.bolton.spaws.model.impl.Submission;
 import uk.ac.edukapp.model.Comment;
 import uk.ac.edukapp.model.Useraccount;
 import uk.ac.edukapp.model.Userreview;
 import uk.ac.edukapp.model.Widgetprofile;
+import uk.ac.edukapp.server.configuration.SpawsServerConfiguration;
 
 public class UserReviewService extends AbstractService {
+	
+	private Logger logger = Logger.getLogger(UserReviewService.class);
 
 	public UserReviewService(ServletContext ctx) {
 		super(ctx);
@@ -42,8 +54,20 @@ public class UserReviewService extends AbstractService {
 		Query wpQuery = entityManager
 				.createNamedQuery("Userreview.findForWidgetProfile");
 		wpQuery.setParameter("widgetprofile", widgetProfile);
-		List<Userreview> reviews = (List<Userreview>) wpQuery.getResultList();
+		List<Userreview> results = (List<Userreview>) wpQuery.getResultList();
 		entityManager.close();
+		
+		List<Userreview> reviews = new ArrayList<Userreview>();
+		reviews.addAll(results);
+		
+		if (SpawsServerConfiguration.getInstance().isEnabled()){
+			try {
+				getExternalReviews(widgetProfile, reviews);
+			} catch (Exception e) {
+				logger.warn("Problem loading external reviews using SPAWS:", e);
+			}
+		}
+		
 		return reviews;
 	}
 
@@ -67,7 +91,49 @@ public class UserReviewService extends AbstractService {
 		userReview.setComment(comment);
 
 		entityManager.getTransaction().commit();
+		
+		//
+		// Sync the review
+		//
+		if (SpawsServerConfiguration.getInstance().isEnabled())
+			publishUserReviewUsingSpaws(userReview);
+		
 		return true;
+	}
+
+	private void publishUserReviewUsingSpaws(Userreview review){
+		try {
+			ParadataManager manager = SpawsServerConfiguration.getInstance().getParadataManager();
+			ISubmission submission = new Submission(new Actor(review.getUserAccount().getUsername()), new Review(review.getComment().getCommenttext()), review.getWidgetProfile().getWidId());
+			ArrayList<ISubmission> submissions = new ArrayList<ISubmission>();
+			submissions.add(submission);
+			manager.publishSubmissions(submissions);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void getExternalReviews(Widgetprofile profile, List<Userreview> reviews) throws Exception{
+		ParadataManager manager = SpawsServerConfiguration.getInstance().getParadataManager();
+		
+		List<ISubmission> submissions = manager.getExternalSubmissions(profile.getWidId(), IReview.VERB);
+		for (ISubmission submission: submissions){
+			Userreview review = new Userreview();
+			Comment comment = new Comment();
+			comment.setCommenttext(submission.getAction().getContent());
+			
+			Useraccount user = new Useraccount();
+			user.setUsername(submission.getActor().getName());
+			// TODO add a URL property for profile page link
+			
+			review.setComment(comment);
+			review.setWidgetProfile(profile);
+			review.setTime(submission.getUpdated());
+			review.setUserAccount(user);
+			
+			reviews.add(review);
+		}
 	}
 
 }
