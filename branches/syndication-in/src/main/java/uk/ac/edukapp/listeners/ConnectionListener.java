6423d8946a3e8d88ecf54bbda850b547aa94aa69
gvanderploeg@iprofs.nl <gvanderploeg@iprofs.nl@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2012-08-23 15:13:53 +0000

    First work on syndication-in

--- branches/syndication-in/src/main/java/uk/ac/edukapp/listeners/ConnectionListener.java
+++ branches/syndication-in/src/main/java/uk/ac/edukapp/listeners/ConnectionListener.java
@@ -1,17 +1,28 @@
 package uk.ac.edukapp.listeners;
 
-import javax.persistence.*;
-import javax.servlet.*;
- 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import uk.ac.edukapp.service.SyndicationScheduler;
+
 public class ConnectionListener implements ServletContextListener {
 
-    //init EntityManagerFactory and store it in servlet context
-    public void contextInitialized(ServletContextEvent e) {        
-        EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("edukapp");
-        
-        e.getServletContext().setAttribute("emf", emf);     
-        
+
+  private static final String SYNDICATION_SCHEDULER_ATTRIBUTE = "syndicationScheduler";
+
+  //init EntityManagerFactory and store it in servlet context
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+      final ServletContext servletContext = servletContextEvent.getServletContext();
+
+      EntityManagerFactory emf = Persistence.createEntityManagerFactory("edukapp");
+      servletContext.setAttribute("emf", emf);
+
+      final SyndicationScheduler syndicationScheduler = new SyndicationScheduler(servletContext);
+      servletContext.setAttribute(SYNDICATION_SCHEDULER_ATTRIBUTE, syndicationScheduler);
+      syndicationScheduler.startPeriodicSyndication();
     }
  
     // Release the EntityManagerFactory:
@@ -19,6 +30,9 @@ public class ConnectionListener implements ServletContextListener {
         EntityManagerFactory emf =
             (EntityManagerFactory)e.getServletContext().getAttribute("emf");
         emf.close();
+
+      SyndicationScheduler syndicationScheduler = (SyndicationScheduler) e.getServletContext().getAttribute(SYNDICATION_SCHEDULER_ATTRIBUTE);
+      syndicationScheduler.stopSchedule();
     }
 }
 
---
+++ branches/syndication-in/src/main/java/uk/ac/edukapp/model/SyndicatedWidgetprofile.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2012 SURFnet bv, The Netherlands
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package uk.ac.edukapp.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+@Entity
+@Table(name = "syndicatedwidgetprofiles")
+public class SyndicatedWidgetprofile implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  public SyndicatedWidgetprofile() {
+  }
+
+  public SyndicatedWidgetprofile(String sourceUri, String name, String license, String author, String widgetId,
+                                 String description, String uri, Date updated, Date created) {
+    this.sourceUri = sourceUri;
+    this.name = name;
+    this.license = license;
+    this.author = author;
+    this.widgetId = widgetId;
+    this.description = description;
+    this.uri = uri;
+    this.updated = updated;
+    this.created = created;
+  }
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private int id;
+
+  @Column
+  private String sourceUri;
+
+  @Column
+  private String name;
+
+  @Column
+  private String license;
+
+  @Column
+  private String author;
+
+  @Column
+  private String widgetId;
+
+  @Column
+  private String description;
+
+  @Column
+  private String uri;
+
+  @Column
+  private Date updated;
+
+  @Column
+  private Date created;
+
+  @Override
+  public String toString() {
+    return new ReflectionToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE).toString();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getWidgetId() {
+    return widgetId;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getUri() {
+    return uri;
+  }
+}
