81682322ea1ee911c21af66642a984b5dafc936f
kjpopat@gmail.com <kjpopat@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2014-02-14 09:02:39 +0000

    Listing Users, user registration, user update, user favourites. Fixes closing entity manager where required.

--- trunk/src/main/java/uk/ac/edukapp/service/UserAccountService.java
+++ trunk/src/main/java/uk/ac/edukapp/service/UserAccountService.java
@@ -15,11 +15,31 @@
  */
 package uk.ac.edukapp.service;
 
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
+import uk.ac.edukapp.model.Accountinfo;
+import uk.ac.edukapp.model.LtiProvider;
 import uk.ac.edukapp.model.Useraccount;
+import uk.ac.edukapp.model.WidgetFavourite;
+import uk.ac.edukapp.model.Widgetprofile;
+import uk.ac.edukapp.servlets.pojos.Users;
+import uk.ac.edukapp.util.MD5Util;
+import uk.ac.edukapp.util.Message;
 
 /**
  * Service for obtaining user accounts
@@ -29,10 +49,34 @@ import uk.ac.edukapp.model.Useraccount;
  */
 public class UserAccountService extends AbstractService {
 
+	private static final Log log = LogFactory.getLog(Users.class);
+	private ServletContext ctx;
+
 	public UserAccountService(ServletContext servletContext) {
 		super(servletContext);
+		ctx = servletContext;
+	}
+	
+	
+	public List<Useraccount>listUsers()
+	{
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		Query q = em.createQuery("SELECT u FROM Useraccount u");
+		@SuppressWarnings("unchecked")
+		List<Useraccount> results = q.getResultList();
+		em.close();
+		return results;
 	}
 
+	/*
+	 * 		EntityManager em = getEntityManagerFactory().createEntityManager();
+		// em.getTransaction().begin();
+		Query q = em.createQuery("SELECT w FROM Widgetprofile w ");
+		@SuppressWarnings("unchecked")
+		List<Widgetprofile> results = q.getResultList();
+		em.close();
+		return results;
+	 */
 	public Useraccount getUserAccount(String username) {
 		Useraccount userAccount = null;
 		EntityManager entityManager = getEntityManagerFactory()
@@ -54,6 +98,7 @@ public class UserAccountService extends AbstractService {
 		EntityManager entityManager = getEntityManagerFactory()
 				.createEntityManager();
 		userAccount = entityManager.find(Useraccount.class, userId);
+		entityManager.close();
 		return userAccount;
 	}
 
@@ -62,6 +107,132 @@ public class UserAccountService extends AbstractService {
 		EntityManager entityManager = getEntityManagerFactory()
 				.createEntityManager();
 		userAccount = entityManager.find(Useraccount.class, userId);
+		entityManager.close();
 		return userAccount;
+	}
+	
+	
+	public Useraccount registerNewUser ( String username, String email, String password, String realname ) throws Exception {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+
+		em.getTransaction().begin();
+
+		Useraccount user = new Useraccount();
+		user.setUsername(username);
+		user.setEmail(email);
+
+		UUID token = UUID.randomUUID();
+		String salt = token.toString();
+		String hashedPassword = MD5Util.md5Hex(salt + password);
+		user.setPassword(hashedPassword);
+		user.setSalt(salt);
+		user.setToken("03");
+		em.persist(user);
+		log.info("User created with id: " + user.getId());
+		Accountinfo info = new Accountinfo();
+		info.setId(user.getId());
+		info.setRealname(realname);
+		info.setJoined(new Timestamp(new Date().getTime()));
+		em.persist(info);
+		//LtiProvider lti = new LtiProvider(user);
+		//em.persist(lti);
+
+		em.getTransaction().commit();
+		em.close();
+
+		ActivityService as = new ActivityService(ctx);
+		as.addUserActivity(user.getId(), "joined", 0);
+
+		return user;
+	}
+	
+	
+	public Useraccount updateUser ( int userId,
+									String username,
+									String email,
+									String password,
+									String realname ) {
+		
+		Useraccount user = this.getUserAccount(userId);
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		user.setEmail(email);
+
+		UUID token = UUID.randomUUID();
+		String salt = token.toString();
+		String hashedPassword = MD5Util.md5Hex(salt + password);
+		user.setPassword(hashedPassword);
+		user.setSalt(salt);
+		em.persist(user);
+		
+		user.setUsername(username);
+		
+		Accountinfo info = user.getAccountInfo();
+		info.setRealname(realname);
+		em.persist(user);
+		em.persist(info);
+		
+		em.getTransaction().commit();
+		em.close();
+		
+		return user;
+	}
+		
+	
+	public Useraccount authenticateUser ( String username, String password ) throws AuthenticationException {
+	    UsernamePasswordToken token = new UsernamePasswordToken( username, password );
+	    Subject currentUser = SecurityUtils.getSubject();
+
+		currentUser.login(token);
+		return getUserAccount ( username );
+	}
+	
+	
+	public WidgetFavourite addFavourite ( Useraccount user, Widgetprofile favourite, int level ) {
+		EntityManager em = getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		WidgetFavourite widgetFavourite = null;
+		// check to see if it exists
+		try {
+			Query q = em.createNamedQuery("favourite.select");
+			q.setParameter("user", user);
+			q.setParameter("widgetprofile", favourite);
+			widgetFavourite = (WidgetFavourite)q.getSingleResult();
+			if ( widgetFavourite != null ) {
+				widgetFavourite.setRelevance(level);
+			}
+		}
+		catch ( NoResultException exp ) {
+			// fine just make one
+			widgetFavourite = new WidgetFavourite();
+			widgetFavourite.setUserAccount(user);
+			widgetFavourite.setWidgetProfile(favourite);
+			widgetFavourite.setRelevance(level);
+			em.persist(widgetFavourite);
+		}
+		em.getTransaction().commit();
+		em.close();
+		return widgetFavourite;
+	}
+	
+	
+	public Message removeFavourite( Useraccount user, Widgetprofile favourite ) {
+		EntityManager em = this.getEntityManagerFactory().createEntityManager();
+		em.getTransaction().begin();
+		Query q = em.createNamedQuery("favourite.select");
+		q.setParameter("user", user);
+		q.setParameter("widgetprofile", favourite);
+		WidgetFavourite fav = (WidgetFavourite) q.getSingleResult();
+		Message msg = new Message();
+		if ( fav != null ) {
+			em.remove(fav);
+			msg.setMessage("OK");
+		}
+		else {
+			msg.setMessage("Favourite could not be removed");
+		}
+		em.getTransaction().commit();
+		em.close();
+		return msg;
 	}
 }
