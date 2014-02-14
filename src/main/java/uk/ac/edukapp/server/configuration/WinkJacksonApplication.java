503128753cb1b5f4c0ed282c2ba32ccfbcdc31ff
kjpopat@gmail.com <kjpopat@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2014-02-14 09:30:51 +0000

    Addition of the pojos

--- src/main/java/uk/ac/edukapp/server/configuration/WinkJacksonApplication.java
+++ src/main/java/uk/ac/edukapp/server/configuration/WinkJacksonApplication.java
@@ -1,3 +1,18 @@
+/*
+ *  (c) 2012 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.edukapp.server.configuration;
 
 import java.util.HashSet;
@@ -19,7 +34,16 @@ public class WinkJacksonApplication extends Application {
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> services = new HashSet<Class<?>>();
-		services.add(uk.ac.edukapp.servlets.Widgets.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Widgets.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Users.class);
+		//services.add(uk.ac.edukapp.servlets.pojos.Annotations.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Creator.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Reviews.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Tags.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Ratings.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Functionalities.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Stats.class);
+		services.add(uk.ac.edukapp.servlets.pojos.Categories.class);
 		return services;
 	}
 
