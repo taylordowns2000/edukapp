4a44e49b249c5d9a643db62ba7ad6886ae3387dd
kjpopat@gmail.com <kjpopat@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2014-02-14 08:54:04 +0000

    Added servlet context as a class property

--- src/main/java/uk/ac/edukapp/service/AbstractService.java
+++ src/main/java/uk/ac/edukapp/service/AbstractService.java
@@ -6,12 +6,14 @@ import javax.servlet.ServletContext;
 public abstract class AbstractService {
 
 	protected EntityManagerFactory emf;
+	protected ServletContext servletContext;
 	
 	public AbstractService(){
 	}
 	
 	public AbstractService(ServletContext ctx){
 		emf = (EntityManagerFactory) ctx.getAttribute("emf");
+		servletContext = ctx;
 	}
 	
 	protected EntityManagerFactory getEntityManagerFactory(){
