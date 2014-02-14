cefbf4ac8ae5be2198c26ab304b7216513b871a4
kjpopat@gmail.com <kjpopat@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2014-02-14 09:24:09 +0000

    Widget builder functionality

---
+++ trunk/src/main/java/uk/ac/edukapp/builder/WidgetData.java
@@ -0,0 +1,100 @@
+/*
+ *  (c) 2013 University of Bolton
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.edukapp.builder;
+
+public class WidgetData {
+	private String widgetId;
+	private String width;
+	private String height;
+	private String title;
+	private String description;
+	private String html;
+	/**
+	 * @return the widgetId
+	 */
+	public String getWidgetId() {
+		return widgetId;
+	}
+	/**
+	 * @param widgetId the widgetId to set
+	 */
+	public void setWidgetId(String widgetId) {
+		this.widgetId = widgetId;
+	}
+	/**
+	 * @return the width
+	 */
+	public String getWidth() {
+		return width;
+	}
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(String width) {
+		this.width = width;
+	}
+	/**
+	 * @return the height
+	 */
+	public String getHeight() {
+		return height;
+	}
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(String height) {
+		this.height = height;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/**
+	 * @return the html
+	 */
+	public String getHtml() {
+		return html;
+	}
+	/**
+	 * @param html the html to set
+	 */
+	public void setHtml(String html) {
+		this.html = html;
+	}
+	
+	
+}
