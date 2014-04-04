68d0d2feb9fc4abf23c2516a3efd175fc4eb43f7
kjpopat@gmail.com <kjpopat@gmail.com@5fc6d161-ea32-e9bb-d6c6-78504b9a0898>
2014-04-04 08:39:21 +0000

    Small changes to services

--- trunk/src/main/java/uk/ac/edukapp/model/Category.java
+++ trunk/src/main/java/uk/ac/edukapp/model/Category.java
@@ -1,11 +1,18 @@
 package uk.ac.edukapp.model;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 
